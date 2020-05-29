815dc9ed9c6b5887d86e6f05fec869326de00665
adrian.rupala <adrian.rupala@whiteaster.com>
2020-05-29 09:30:24 +0200

    wait-for psql

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/app/scripts/wait_for.sh ofd_agregator_cms_psql:5432 -t 15 -- echo "Database (PSQL) is up!"
+/app/scripts/wait_for.sh ofd_agregator_cms_psql:5432 -t 15 -- echo "Database (ofd_agregator_cms_psql) is up!"
 
 python /app/manage.py buildapp --profile development
 
