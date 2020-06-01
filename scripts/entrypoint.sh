4dcbe65a8049681e7122225278cfc8ea8825489e
Adrian Rupala <adrian.rupala@whiteaster.com>
2020-06-01 21:20:34 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,5 +2,6 @@
 /app/scripts/wait_for.sh ofd_agregator_cms_psql:5432 -t 15 -- echo "Database (ofd_agregator_cms_psql) is up!"
 
 python /app/manage.py buildapp --profile development
+python /app/manage.py collectstatic --noinput
 
 python /app/manage.py runserver 0.0.0.0:8000
