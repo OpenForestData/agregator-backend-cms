9ab0cbf66dd78b5bbfa704e18586e675261db229
Adrian Rupala <adrian.rupala@whiteaster.com>
2020-06-23 16:01:38 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /app/scripts/wait_for.sh ofdAgregatorCMSPsql:5432 -t 15 -- echo "Database (ofdAgregatorCMSPsql) is up!"
 
-python /app/manage.py buildapp --profile development
+python /app/manage.py buildapp --profile settings
 python /app/manage.py collectstatic --noinput
 
 python /app/manage.py runserver 0.0.0.0:8000
