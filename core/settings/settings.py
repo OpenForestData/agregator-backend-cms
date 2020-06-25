e82331cdce9dfaab39da9ba785b332c8eafbc5ee
aleksander.nowak@whiteaster.com <aleksander.nowak@whiteaster.com>
2020-06-25 09:26:07 +0200

    Psycopg2 in requirements add

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -218,11 +218,11 @@ CMS_PLACEHOLDER_CONF = {}
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': os.environ.get('DB_PORT', ''),
+        'NAME': os.environ.get('DB_NAME', 'agregatorcms'),
+        'USER': os.environ.get('DB_USER', 'agregatorcmsusr'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'HhP4gXsH'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
