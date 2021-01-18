3691844c312afb7d2b4e53802db052b43053dac8
aleksander.nowak@whiteaster.com <aleksander.nowak@whiteaster.com>
2021-01-18 08:25:56 +0100

    Buildapp script turn on

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /app/scripts/wait_for.sh ofdAgregatorCMSPsql:5432 -t 15 -- echo "Database (ofdAgregatorCMSPsql) is up!"
 
+python /app/manage.py buildapp
 python /app/manage.py makemigrations
 python /app/manage.py migrate
 python /app/manage.py collectstatic --noinput
